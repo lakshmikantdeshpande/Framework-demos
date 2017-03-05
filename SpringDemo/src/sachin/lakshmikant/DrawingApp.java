8222e27b13f380fc0405785eeba86a19b64bfdcc
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-03-05 14:41:01 +0530

    Constructor, Setter Injection

--- SpringDemo/src/sachin/lakshmikant/DrawingApp.java
+++ SpringDemo/src/sachin/lakshmikant/DrawingApp.java
@@ -1,18 +1,21 @@
 package sachin.lakshmikant;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
 		// Triangle triangle = new Triangle();
 
-		BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource(
-				"spring.xml"));
-		Triangle triangle = (Triangle) beanFactory.getBean("triangle");
+		// BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource(
+		// "spring.xml"));
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"spring.xml");
+		Triangle triangle = (Triangle) ctx.getBean("triangle");
 		triangle.draw();
+
 	}
 
 }
--- SpringDemo/src/sachin/lakshmikant/Triangle.java
+++ SpringDemo/src/sachin/lakshmikant/Triangle.java
@@ -2,8 +2,38 @@ package sachin.lakshmikant;
 
 public class Triangle {
 
+	private String type;
+	private int height;
+
+	public Triangle(String type) {
+		this.type = type;
+	}
+	
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+
+
+	// setter injection
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public void draw() {
-		System.out.println("Triangle drawn");
+		System.out.println(getType() + " triangle drawn of height " + getHeight());
 	}
 
 }
