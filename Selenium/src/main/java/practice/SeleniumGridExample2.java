d3778ae4a34593efa36e2173ee37bbf882c28d0a
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2017-12-23 20:04:36 +0530

    Tests now run on different nodes

--- Selenium/src/main/java/practice/SeleniumGridExample2.java
+++ Selenium/src/main/java/practice/SeleniumGridExample2.java
@@ -2,7 +2,7 @@ package practice;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
@@ -13,17 +13,17 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class SeleniumGridExample {
+public class SeleniumGridExample2 {
 
     private WebDriver webDriver;
     private String baseURL, nodeURL;
 
     @BeforeTest
     public void setup() throws MalformedURLException {
-        DesiredCapabilities desiredCapabilities = DesiredCapabilities.chrome();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setHeadless(true);
-        desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        DesiredCapabilities desiredCapabilities = DesiredCapabilities.firefox();
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setHeadless(true);
+        desiredCapabilities.setCapability(FirefoxOptions.FIREFOX_OPTIONS, firefoxOptions);
         desiredCapabilities.setPlatform(Platform.LINUX);
 
         baseURL = "https://www.google.com";
@@ -33,7 +33,7 @@ public class SeleniumGridExample {
     }
 
     @Test
-    public void simpleTest() {
+    public void duplicateTest() {
         webDriver.get(baseURL);
         Assert.assertTrue(webDriver.getTitle().equals("Google"));
     }
