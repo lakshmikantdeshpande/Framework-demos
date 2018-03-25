7c3d43f393ac8a85ad405adea5dc248d299e7b8c
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-03-25 13:10:06 +0530

    Basics of Service and dependency injection

--- angular/mosh/2_angular-fundamentals/hello-world/src/app/courses.component.ts
+++ angular/mosh/2_angular-fundamentals/hello-world/src/app/courses.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 
 @Component({
     selector: 'courses',
     template: `
-        <h2>{{ getTitle() }}</h2>
+        <h2>{{ title }}</h2>
         <ul>
             <li *ngFor='let course of courses'>
                 {{ course }}
@@ -13,6 +14,10 @@ import { Component } from '@angular/core';
     `
 })
 export class CoursesComponent {
-    private title = "List of courses";
-    courses = ['English', 'Hindi', 'Marathi'];
+    title = "List of courses";
+    courses;
+
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 }
---
+++ angular/mosh/2_angular-fundamentals/hello-world/src/app/courses.service.ts
@@ -0,0 +1,5 @@
+export class CoursesService {
+    getCourses() {
+        return ['English', 'Hindi', 'Marathi'];
+    }    
+}
