bde800b1a009aba8ec64d770bd38f56b5c53ed42
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-03-25 12:53:09 +0530

    ngFor

--- angular/mosh/2_angular-fundamentals/hello-world/src/app/courses.component.ts
+++ angular/mosh/2_angular-fundamentals/hello-world/src/app/courses.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'courses',
-    template: '<h2>{{ getTitle() }}</h2>'
+    template: `
+        <h2>{{ getTitle() }}</h2>
+        <ul>
+            <li *ngFor='let course of courses'>
+                {{ course }}
+            </li>
+        </ul>
+    `
 })
 export class CoursesComponent {
     private title = "List of courses";
-
-    getTitle() {
-        return this.title;
-    }
+    courses = ['English', 'Hindi', 'Marathi'];
 }
