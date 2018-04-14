cf2b89c8216f0b590c7c008fba5308274719031d
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-14 08:46:51 +0530

    click listener added

--- angular/tour/src/app/heroes/heroes.component.ts
+++ angular/tour/src/app/heroes/heroes.component.ts
@@ -16,6 +16,12 @@ export class HeroesComponent implements OnInit {
     name: 'WindStorm'
   };
 
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   constructor() { }
 
   ngOnInit() {
