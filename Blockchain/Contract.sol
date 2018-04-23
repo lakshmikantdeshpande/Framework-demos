aa6dc882cabdd4ed4f4acbcfa97f58befaa301a6
Lakshmikant Deshpande <lakshmikantdeshpande@gmail.com>
2018-04-23 23:32:54 +0530

    Declaring variables in Solidity

--- Blockchain/Contract.sol
+++ Blockchain/Contract.sol
@@ -1,5 +1,13 @@
-pragma solidity  ^0.4.19;
+pragma solidity ^0.4.19;
 
 contract ZombieFactory {
+    
+    // state variable (stored permanantly in the blockchain)
+    uint dnaDigits = 16;
+
+    // ** is power operator (just like python)
+    uint dnaModulus = 10 ** dnaDigits; 
+
 
 }
+
