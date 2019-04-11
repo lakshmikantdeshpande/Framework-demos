bfc0b1382e81de9b1341a9871e08add94f2fd30b
Lakshmikant Deshpande <lakshmikantdeshpadne@gmail.com>
2019-04-11 14:57:04 +0530

    Completed 'Displaying a List'

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { HEROES } from '../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
@@ -8,10 +9,12 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
